c98a470022a3c436b0e1fb8d91004a21d6d426f9
dwiyudha0444 <yudhacamo12@gmail.com>
2023-11-20 14:05:59 +0700

    first commit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,15 @@
+@include('admin.head')
+
+<body>
+    @include('admin.navbar')
+    @include('admin.sidebar')
+
+    @yield('content')
+
+  @include('admin.footer')
+
+  @include('admin.script')
+
+</body>
+
+</html>
